38d23df7f718a9e1fbc86109fa9ee3d9c05fff11
Petar Kostic <pjotrjk2@gmail.com>
2025-07-06 17:02:21 +0200

    Initial commit -> setup basic env and makefile, launches etcetera

---
+++ README.md
@@ -0,0 +1,15 @@
+# Pong
+Attempt at focusing on basic gamedev hooks.
+
+Uses raylib for graphics.
+
+# TODO list
+1. Initialize blank screen and game loop, setup Makefile and basic structure
+2. Draw the ball and the rackets
+3. Move the ball around
+4. Check for a collision with edges
+5. Player movement
+6. AI movement
+7. Check for a collision with rackets
+8. Scoring?
+9. Refactor until happy
---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <raylib.h>
+
+int main() 
+{
+    const Color darkGreen = {20, 160, 133, 255};
+    
+    constexpr int screenWidth = 800;
+    constexpr int screenHeight = 600;
+    
+    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
+    SetTargetFPS(60);
+    
+    while (!WindowShouldClose())
+    {        
+        BeginDrawing();
+        ClearBackground(darkGreen);
+        EndDrawing();
+    }
+    
+    CloseWindow();
+}
