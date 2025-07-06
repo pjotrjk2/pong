ad815ee0a8dfdce53fd486b95e16bc56da0c27c2
Petar Kostic <pjotrjk2@gmail.com>
2025-07-06 17:25:11 +0200

    Primitive ball class, drawn the court

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,69 @@
+#include <iostream>
 #include <raylib.h>
 
-int main() 
+class Ball
 {
-    const Color darkGreen = {20, 160, 133, 255};
+    public:
+        float x;
+        float y;
+        int speedX, speedY;
+        int radius;
+
+        Ball(float x, float y, int speedX = 0, int speedY = 0, int radius = 20)
+        {
+            this->x = x;
+            this->y = y;
+            this->speedX = speedX;
+            this->speedY = speedY;
+            this->radius = radius;
+        }
+
+        void Draw() 
+        {
+            DrawCircle(x, y, radius, WHITE);
+        }
+
+        void Update()
+        {
+            x += speedX;
+            y += speedY;
+        }
+};
+
+int main() 
+{    
+    const int screenWidth = 1280;
+    const int screenHeight = 800;
     
-    constexpr int screenWidth = 800;
-    constexpr int screenHeight = 600;
-    
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
+    InitWindow(screenWidth, screenHeight, "Perica's pong game");
     SetTargetFPS(60);
     
+    // Initialize objects
+    Ball ball(screenWidth / 2, screenHeight / 2, 7, 7, 20);
+
     while (!WindowShouldClose())
     {        
         BeginDrawing();
-        ClearBackground(darkGreen);
+
+        // UPDATE
+        ball.Update();
+
+        
+        ClearBackground(BLACK);
+
+        // Draw line at half court
+        DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, WHITE);
+        
+        // Draw ball
+        ball.Draw();
+
+        // Draw rackets
+        DrawRectangle(10, screenHeight / 2 - 60, 25, 120, WHITE);
+        DrawRectangle(screenWidth - 35, screenHeight / 2 - 60, 25, 120, WHITE);
+
         EndDrawing();
     }
     
     CloseWindow();
+    return 0;
 }
